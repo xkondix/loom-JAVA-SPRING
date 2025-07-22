5957c011a074c2df74745260b634331d8a258d63
Konrad <konrad.kowalczyk.98@gmail.com>
2025-07-22 20:34:58 +0200

    Initial commit

---
+++ src/main/java/com/kowalczyk/konrad/loom/LoomApplication.java
@@ -0,0 +1,13 @@
+package com.kowalczyk.konrad.loom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoomApplication.class, args);
+	}
+
+}
